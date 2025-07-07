57b0a6093abcdf2f33afc2fe5d919b047a471a6c
Alexandru Trifu-Urzica <alexandru.trifu-urzica@softwire.com>
2025-07-07 14:47:27 +0300

    implemented getAvailableMoves()

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,21 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let availableMoves: Square[] = []
+        let currentSquare = board.findPiece(this)
+
+        for (let index = 0; index < 8; index++) {
+            // Horizontal moves
+            if (index != currentSquare.col) {
+                availableMoves.push(new Square(currentSquare.row, index))
+            }
+
+            // Vertical moves
+            if (index != currentSquare.row) {
+                availableMoves.push(new Square(index, currentSquare.col))
+            }
+        }
+
+        return availableMoves
     }
 }
