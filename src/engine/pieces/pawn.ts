6ad33081a09a47b2cac4243c938a58f6bfadf751
Alexandru Trifu-Urzica <alexandru.trifu-urzica@softwire.com>
2025-07-07 14:36:46 +0300

    overrode moveTo() to update firstMove field

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -4,6 +4,8 @@ import Board from '../board';
 import Square from "../square";
 
 export default class Pawn extends Piece {
+    private firstMove: boolean = true
+
     public constructor(player: Player) {
         super(player);
     }
@@ -14,10 +16,23 @@ export default class Pawn extends Piece {
 
         if (this.player == Player.WHITE) {
             availableMoves.push(new Square(currentSquare.row + 1, currentSquare.col))
+
+            if (this.firstMove) {
+                availableMoves.push(new Square(currentSquare.row + 2, currentSquare.col))
+            }
         } else {
             availableMoves.push(new Square(currentSquare.row - 1, currentSquare.col))
+
+            if (this.firstMove) {
+                availableMoves.push(new Square(currentSquare.row - 2, currentSquare.col))
+            }
         }
 
         return availableMoves
+    }
+
+    moveTo(board: Board, newSquare: Square) {
+        super.moveTo(board, newSquare);
+        this.firstMove = false
     }
 }
