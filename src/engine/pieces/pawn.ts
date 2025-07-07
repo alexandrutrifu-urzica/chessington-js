0c3ef0d367f9527c6a804a26a61b53c87ac092bd
Alexandru Trifu-Urzica <alexandru.trifu-urzica@softwire.com>
2025-07-07 12:57:54 +0300

    fixed red-1 (pawn)

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,15 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let availableMoves: Square[] = []
+        let currentSquare = board.findPiece(this)
+
+        if (this.player == Player.WHITE) {
+            availableMoves.push(new Square(currentSquare.row + 1, currentSquare.col))
+        } else {
+            availableMoves.push(new Square(currentSquare.row - 1, currentSquare.col))
+        }
+
+        return availableMoves
     }
 }
