8878deb6abb661dab8a8004639d23b164b72f6c5
Alexandru Trifu-Urzica <alexandru.trifu-urzica@softwire.com>
2025-07-07 15:38:09 +0300

    implemented getAvailableMoves() for king

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,25 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const steps = [-1, 0, 1]
+        const currentSquare = board.findPiece(this)
+        let availableMoves: Square[] = []
+
+        for (let rowAdjustment of steps) {
+            for (let colAdjustment of steps) {
+                if (rowAdjustment == 0 && colAdjustment == 0) {
+                    continue
+                }
+
+                let rowIndex = currentSquare.row + rowAdjustment
+                let colIndex = currentSquare.col + colAdjustment
+
+                if (this.isValidIndex(rowIndex) && this.isValidIndex(colIndex)) {
+                    availableMoves.push(new Square(rowIndex, colIndex))
+                }
+            }
+        }
+
+        return availableMoves
     }
 }
