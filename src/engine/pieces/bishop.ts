aba2437e9412ed9b1272d91d7bc14e74524f131e
Alexandru Trifu-Urzica <alexandru.trifu-urzica@softwire.com>
2025-07-07 15:13:57 +0300

    implemented getAvailableMoves() for bishop

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,26 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let availableMoves: Square[] = []
+        let currentSquare = board.findPiece(this)
+
+        const directions = [-1, 1]
+        const isValidIndex = (index: number) => (0 <= index) && (index < 8)
+
+        for (let rowAdjustment of directions) {
+            for (let colAdjustment of directions) {
+                let rowIndex = currentSquare.row + rowAdjustment
+                let colIndex = currentSquare.col + colAdjustment
+
+                while (isValidIndex(rowIndex) && isValidIndex(colIndex)) {
+                    availableMoves.push(new Square(rowIndex, colIndex))
+
+                    rowIndex += rowAdjustment
+                    colIndex += colAdjustment
+                }
+            }
+        }
+
+        return availableMoves
     }
 }
